0c7cc004efd5d2672d3ab09e3627c5f9be197ab5
Ramond Falcão <ramondfalcao@gmail.com>
2022-08-27 11:17:22 -0300

    Feat: Criando model Team

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'team',
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
