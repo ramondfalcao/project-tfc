372237a93da0a6d8d9241fa51b201234854bdf7b
Ramond Falcão <ramondfalcao@gmail.com>
2022-08-27 11:13:29 -0300

    Push Para testar avaliador remoto

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import UserService from '../services/userService';
 
-class UserController {
+export default class UserController {
   constructor(private userService: UserService) {}
 
   async login(req: Request, res: Response): Promise<void> {
@@ -11,11 +11,9 @@ class UserController {
   }
 
   async loginValidate(req: Request, res: Response): Promise<void> {
-    const token = String(req.headers.authorization);
+    const token = req.headers.authorization;
     const tokenToString = String(token);
     const role = await this.userService.loginValidate(tokenToString);
     res.status(200).json({ role });
   }
 }
-
-export default UserController;
--- app/backend/src/services/jwtService.ts
+++ app/backend/src/services/jwtService.ts
@@ -1,16 +1,15 @@
-import { Secret, sign, verify } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
+import 'dotenv/config';
 import User from '../interfaces/IUser';
-
-const JWT_SECRET: Secret = 'jwt_secret';
 
 class JwtService {
   static sign(data: { email: string, password: string }): string {
-    return sign({ data }, JWT_SECRET);
+    return sign({ data }, process.env.JWT_SECRET || 'jwt_secret');
   }
 
   static tokenValidation(token: string) {
     try {
-      const data = verify(token, JWT_SECRET);
+      const data = verify(token, process.env.JWT_SECRET || 'jwt_secret');
       return data as User;
     } catch (error) {
       const err = new Error('Token must be a valid token');
